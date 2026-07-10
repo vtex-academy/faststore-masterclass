bd5040225465bb730973e64a125fdd1770937d4b
VTEX Academy <academy@vtex.com>
2026-07-10 13:37:00 -0300

    SalesChannel as stringchannel: '{"salesChannel":"1","regionId":""}',

--- discovery.config.js
+++ discovery.config.js
@@ -29,7 +29,7 @@ module.exports = {
       symbol: "R$",
     },
     locale: "pt-BR",
-    channel: '{"salesChannel":1,"regionId":""}',
+    channel: '{"salesChannel":"1","regionId":""}',
     country: "BRA",
     deliveryMode: null,
     addressType: null,
